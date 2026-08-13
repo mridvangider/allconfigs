020da628959d3b558596fefd4e565e2cd1826a44
Muhammed Ridvan Gider <mridvang@gmail.com>
2026-08-13 06:57:01 +0300

    fix install.sh creating self-referential links on re-run (ln -sfn)

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -sf "$SCRIPT_DIR/.bashrc" "$HOME/.bashrc"
-ln -sf "$SCRIPT_DIR/.bashrc.d" "$HOME/.bashrc.d"
+ln -sfn "$SCRIPT_DIR/.bashrc.d" "$HOME/.bashrc.d"
 mkdir -p "$HOME/.config"
-ln -sf "$SCRIPT_DIR/nvim" "$HOME/.config/nvim"
+ln -sfn "$SCRIPT_DIR/nvim" "$HOME/.config/nvim"
 ln -sf "$SCRIPT_DIR/.tmux.conf" "$HOME/.tmux.conf"
 
 # Alacritty
